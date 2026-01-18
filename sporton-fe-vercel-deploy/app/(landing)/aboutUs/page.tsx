2c056bfed9533ad34b3dd62c7f46af10a8b75091
arishanzo <arishanzo@gmail.com>
2026-01-18 17:33:34 +0700

    Live Session 2 - Add landing page components for checkout, order status, and payment processes

---
+++ sporton-fe-vercel-deploy/app/(landing)/aboutUs/page.tsx
@@ -0,0 +1,9 @@
+const aboutUs = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-primary">Halo ini Halaman About Us</h1>
+    </main>
+  );
+};
+
+export default aboutUs
